17b157bc6f4710b700f41d2f7d661bf912930661
jmrangelSTEMDO <jmrangel@stemdo.io>
2025-02-06 12:36:33 +0100

    changed script start

--- app/index.js
+++ app/index.js
@@ -12,8 +12,8 @@ const supabaseUrl = 'https://cmzgqyfvwnsxrbinxkjk.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtemdxeWZ2d25zeHJiaW54a2prIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczODU3MjU5MCwiZXhwIjoyMDU0MTQ4NTkwfQ.DSRew9cWsqX22HZIR6JkxDr5F7lAdxQbT4R5XqYYaUA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-// const CryptoJS = require('crypto-js');
-// const SECRET_KEY='0U370okyNsPAQ1nMnvy21Mu7MGi/8KbnNu9gfz14IX8=';
+const CryptoJS = require('crypto-js');
+const SECRET_KEY='0U370okyNsPAQ1nMnvy21Mu7MGi/8KbnNu9gfz14IX8=';
 
 app.get("/", (req, res) => {
     res.send("bienvenido a mi api");
@@ -23,7 +23,7 @@ app.post("/api/usuarios", async(req, res) => {
     if (validarEmail(req.body.email)) {
         emailD = req.body.email;
       try {
-        // const encryptedData = encryptData(req.body.email);
+        const encryptedData = encryptData(req.body.email);
         const { data, error } = await supabase
           .from('usuarios')
           .insert([{ email: emailD }])
@@ -44,9 +44,9 @@ app.listen(port, () =>{
     console.log(`port running in http://localhost:${port}`);
 });
 
-// function encryptData(data) {
-//     return CryptoJS.AES.encrypt(data,'0U370okyNsPAQ1nMnvy21Mu7MGi/8KbnNu9gfz14IX8=').toString();
-//   }
+function encryptData(data) {
+    return CryptoJS.AES.encrypt(data,'0U370okyNsPAQ1nMnvy21Mu7MGi/8KbnNu9gfz14IX8=').toString();
+  }
 
 function validarEmail(email) {
     return emailRegex.test(email);
